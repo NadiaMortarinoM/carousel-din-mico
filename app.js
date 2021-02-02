b61f550ba6ab231be72accaae187b769de031383
Nadia Mortarino Martinez <56925951+NadiaMortarinoM@users.noreply.github.com>
2021-02-02 10:39:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function(){
+    new Glider (document.querySelector('.carousel__lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+  },
+  //Glider.JS (https://nickpiscitelli.github.io/Glider.js/)
+  responsive: [
+    {
+      // screens greater than >= 450px
+      breakpoint: 450,
+      settings: {
+        // Set to 2 and provide item width to adjust to viewport
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },{
+      // screens greater than >= 800px
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    }
+  ]
+    });
+});
